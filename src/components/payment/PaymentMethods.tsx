2e520b041c0fc02d8da4235572172f67840037b4
streamline-autmations <streamline.automations.hq@gmail.com>
2025-10-14 17:21:30 +0200

    Fix missing Instant EFT payment method image
    - Updated image path from '/instantEFT.svg' to '/instantEFT (1).svg'
    - All payment method images now properly display in the 'We accept the following payment methods' section

--- src/components/payment/PaymentMethods.tsx
+++ src/components/payment/PaymentMethods.tsx
@@ -12,7 +12,7 @@ export const PaymentMethods: React.FC = () => {
           <img src="/applepay.svg" alt="Apple Pay" className="h-8 w-12 object-contain bg-white border border-gray-200 rounded-md p-1" />
           <img src="/credit-card.svg" alt="Credit Card" className="h-8 w-12 object-contain bg-white border border-gray-200 rounded-md p-1" />
           <img src="/debit-card.svg" alt="Debit Card" className="h-8 w-12 object-contain bg-white border border-gray-200 rounded-md p-1" />
-          <img src="/instantEFT.svg" alt="Instant EFT" className="h-8 w-12 object-contain bg-white border border-gray-200 rounded-md p-1" />
+          <img src="/instantEFT (1).svg" alt="Instant EFT" className="h-8 w-12 object-contain bg-white border border-gray-200 rounded-md p-1" />
           <img src="/mobicred.svg" alt="Mobicred" className="h-8 w-12 object-contain bg-white border border-gray-200 rounded-md p-1" />
           <img src="/moretyme.svg" alt="MoreTyme" className="h-8 w-12 object-contain bg-white border border-gray-200 rounded-md p-1" />
           <img src="/samsungpay.svg" alt="Samsung Pay" className="h-8 w-12 object-contain bg-white border border-gray-200 rounded-md p-1" />
